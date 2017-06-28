4e052a0cfde9720922f9075da4427a37d7ca7816
Jin <imreallyjin@gmail.com>
2017-06-28 13:54:29 -0700

    Couple of things
    1. On the iPhone Simulator, the "Take Photo from Camera" is not added to tue UIAlertController
    
    2. When I run the app on the phone, it doesn't work due to error.
    
    oh dayum

--- MainTabBarController.swift
+++ MainTabBarController.swift
@@ -9,8 +9,18 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
+    
+    // MARK: - Properties
+    let photoHelper = MGPhotoHelper()
+    
+    
+    // MARK: - Override func
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        photoHelper.completionHandler = { image in
+            print("handle image")
+        }
         
         delegate = self
         tabBar.unselectedItemTintColor = .black
@@ -20,6 +30,7 @@ class MainTabBarController: UITabBarController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     /*
     // MARK: - Navigation
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -28,18 +39,16 @@ class MainTabBarController: UITabBarController {
         // Pass the selected object to the new view controller.
     }
     */
+    
 }
 
 extension MainTabBarController: UITabBarControllerDelegate {
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
         if viewController.tabBarItem.tag == 1 {
-            // present photo taking action sheet
-            print("take photo")
-            
+            photoHelper.presentActionSheet(from: self)
             return false
-        } else {
-            return true
         }
+        return true
     }
 }
 
